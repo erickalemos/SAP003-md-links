6504655cfad9ca166d18da6be67fec23b915684a
erickalemos <ericka.florentino@gmail.com>
2019-12-03 16:27:51 -0300

    atualização do readme

--- cli.js
+++ cli.js
@@ -5,7 +5,6 @@ const path = process.argv[2]
 
 mdLinks(path)
     .then((parametro)=>{
-        //console.log(parametro)
         parametro.forEach(edition => console.log(`texto: ${edition.titulo }\n link: ${edition.link}`))
     })
     .catch(console.error);
