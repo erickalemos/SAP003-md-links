f3157e604754fb04de587436d77454f11f737059
erickalemos <ericka.florentino@gmail.com>
2019-12-03 09:07:28 -0300

    atualização da pasta do cli

---
+++ cli.js
@@ -0,0 +1,11 @@
+#! /usr/bin/env node
+const mdLinks = require('../SAP003-md-links/lib/index.js');
+
+const path = process.argv[2]
+
+mdLinks(path)
+    .then((parametro)=>{
+        //console.log(parametro)
+        parametro.forEach(edition => console.log(`texto: ${edition.titulo }\n link: ${edition.link}`))
+    })
+    .catch(console.error);
